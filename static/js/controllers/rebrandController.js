7eab5f132eb10fd50a6408c1f956b4049818456c
losethequit <losethequit@gmail.com>
2018-12-09 21:17:17 -0800

    moving to a dev001

---
+++ static/js/controllers/rebrandController.js
@@ -0,0 +1,42 @@
+"use strict";
+
+console.log("outer rebrandController");
+
+angular
+  .module("shell")
+  .controller("rebrandController", function($scope, shellService) {
+    console.log("rebrandController initialized!");
+    $scope.trafficSpy = function(x) {
+      var dataToSend = {};
+
+      $.get("https://api.ipify.org?format=json", function(data, error) {
+        dataToSend.site = "LTQ";
+        dataToSend.ip = data.ip;
+        dataToSend.referrer = document.referrer;
+        dataToSend.vendor = navigator.vendor;
+        dataToSend.userAgent = navigator.userAgent;
+        dataToSend.platform = navigator.platform;
+        dataToSend.language = navigator.language;
+        dataToSend.hostname = window.location.hostname;
+
+        if (document.referrer) {
+          dataToSend.referrer = document.referrer;
+        } else {
+          dataToSend.referrer = "Direct";
+        }
+
+        // if (error === 'success') {
+        // if (error === 'success' && window.location.hostname !== 'localhost') {
+        if (error === "success") {
+          console.log(`traffic-spy`);
+          console.log(error);
+
+          shellService.trafficSpy(dataToSend, function(response) {
+            console.log(response);
+          });
+        }
+      });
+    };
+
+    $scope.trafficSpy();
+  });
--- static/js/shell.js
+++ static/js/shell.js
@@ -1,3 +1,44 @@
-'use strict';
+"use strict";
 
-angular.module("shell", []);
+console.log("OUTSIDE: main");
+const app = angular
+  .module("shell", [
+    "ngRoute",
+    "ngMaterial",
+    "ngAnimate",
+    "ngAria",
+    "ngMessages",
+    "ngCookies",
+    "ngLodash"
+  ])
+  .config(function($routeProvider, $mdThemingProvider, $mdAriaProvider) {
+    $mdThemingProvider
+      .theme("default")
+      .primaryPalette("grey")
+      .accentPalette("indigo");
+
+    // $routeProvider.when("/home", {
+    //   templateUrl: "../templates/inject.html",
+    //   controller: "alloyController"
+    // });
+
+    $routeProvider.when("/home", {
+      templateUrl: "../templates/portfolio.html",
+      controller: "rebrandController"
+    });
+
+    $routeProvider.when("/rebrand", {
+      templateUrl: "../templates/rebrand.html",
+      controller: "rebrandController"
+    });
+
+    $routeProvider.when("/resume", {
+      templateUrl: "../templates/instagram.html",
+      controller: "alloyController"
+    });
+
+    $routeProvider.otherwise({
+      redirectTo: "/home",
+      controller: "alloyController"
+    });
+  });
